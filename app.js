023cc97ff36a10a7c264a9b148d3666b0ee2e85a
Mary Kaki <justonly4testing@gmail.com>
2021-03-24 22:08:58 +0000

    I added code to show any post in separated html page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -39,14 +40,15 @@ app.get("/compose", function(req, res){
 
 
 
+
 app.post("/compose", function(req, res){
 
   const publishedPost = {
-    title: req.body.postTitle,
-    body: req.body.postBody
+    title: req.body.postTitle, // this pulls out the post title from the html and adds in the variable
+    body: req.body.postBody // this pulls out the post body from the html and adds in the variable
   }
 
-  posts.push(publishedPost);
+  posts.push(publishedPost); // 'posts' is an array for every post published that is carried in variable 'publishedPost'
 
 res.redirect("/");
 
@@ -55,13 +57,25 @@ res.redirect("/");
 
 
 app.get("/posts/:postName/", function(req,res){
-  const requestedTitle = req.params.postName;
+  const requestedTitle = _.lowerCase(req.params.postName);
 
   posts.forEach(function (post){
-    const storedTitle = post.title;
+
+    const storedTitle = _.lowerCase(post.title);
+
     if (storedTitle === requestedTitle){
-      console.log("match found");
+      //console.log("match found!");
+
+        res.render("post", {
+          title:post.title,
+          content:post.content
+
+        });
+
+
+
     }
+
   })
 
 });
